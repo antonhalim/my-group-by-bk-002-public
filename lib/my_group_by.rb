45a8842bae71af266a8d42ecf89942ab9291c6e6
axton21 <axton_n@yahoo.com>
2015-04-05 01:58:18 -0400

    done

--- lib/my_group_by.rb
+++ lib/my_group_by.rb
@@ -1,2 +1,11 @@
-# your code goes here
-# you will be adding the method #my_group_by to the Enumerable module (mixin)
+module Enumerable
+	def my_group_by
+		array = self.clone
+		array.each_with_object({}) do |arg, result|
+			res = yield arg
+			result[res] ||= []
+			result[res] << arg
+		end
+	end
+
+end
